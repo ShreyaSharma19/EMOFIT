27e44ed9e832f4a92af7eff00f073489f3ab5d0f
kopalg20 <144939959+kopalg20@users.noreply.github.com>
2024-10-04 23:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,6 @@ from flask_mysqldb import MySQL
 from model import predict_suggestions, fetch_user_data  # Import your functions
 
 app = Flask(__name__)
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
-app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
